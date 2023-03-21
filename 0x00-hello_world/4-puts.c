fdaa421b4cd3f67baf0aa5b6f4956d2944967e06
YOUSEF <yousefrahimi67@gmail.com>
2023-03-20 17:14:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -o cisfun $CFILE
+#include <studio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline/"
+ * return 0,
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
